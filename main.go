2077831b87dddd87ab58f2f2f9919fda240a6e6e
leiwen <657106593@qq.com>
2020-12-30 23:15:31 +0800

    feat：更改etcd

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/registry"
-	"github.com/micro/go-plugins/registry/consul"
+	"github.com/micro/go-micro/registry/etcd"
 	"micro/Services"
 	"micro/ServicesImpl"
 )
 
 func main() {
-	consulReg := consul.NewRegistry(
-		registry.Addrs("192.168.1.104:8500"),
-	)
+	//consulReg := consul.NewRegistry(
+	//	registry.Addrs("192.168.1.104:8500"),
+	//)
+	etcdReg := etcd.NewRegistry(registry.Addrs("192.168.1.104:2379"))
 
 	myService := micro.NewService(
 		micro.Name("micro.micro-api"),
 		micro.Address(":8001"),
-		micro.Registry(consulReg),
+		micro.Registry(etcdReg),
 		)
 
 	Services.RegisterTestServiceHandler(myService.Server(),new(ServicesImpl.TestService))
